c18044c3d9ffeffe63b1a3fff1b0b3b0c4ffe7cb
Simon Larsen <simonhffh@gmail.com>
2019-03-17 20:16:59 +0100

    Added new highscore and jukebox selection images

--- selection_highscore.h
+++ selection_highscore.h
@@ -1,6 +1,6 @@
 #ifndef SELECTION_HIGHSCORE_MAP_H
 #define SELECTION_HIGHSCORE_MAP_H
-#define selection_highscore_data_length 66
+#define selection_highscore_data_length 65
 extern const unsigned char selection_highscore_data[];
 #define selection_highscore_tiles_width 16
 #define selection_highscore_tiles_height 6
