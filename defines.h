1edaf1738cc96144b878a09fa664d85e849dfd39
Simon Larsen <simonhffh@gmail.com>
2019-05-09 20:38:21 +0200

    Removed unsused defines in defines.h

--- defines.h
+++ defines.h
@@ -8,11 +8,6 @@
 #define DOWN	3U
 #define LEFT	4U
 
-#define UPLEFT		5U
-#define UPRIGHT		6U
-#define DOWNLEFT	7U
-#define DOWNRIGHT	8U
-
 // Entity properties
 #define FLIP_X 32U
 #define FLIP_Y 64U
