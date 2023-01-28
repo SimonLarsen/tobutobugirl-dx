c10b0a4906c7818788a6957f7b5b6d612a7088db
Adrian Siekierka <kontakt@asie.pl>
2023-01-28 08:35:06 +0100

    remove unused mymemcpy.asm

--- gamestate.h
+++ gamestate.h
@@ -114,6 +114,5 @@ extern const UBYTE rank_letters[4];
 void get_bkg_palette(const UBYTE *buffer) OLDCALL;
 void get_sprite_palette(const UBYTE *buffer) OLDCALL;
 void mymemset(UBYTE *dest, UBYTE value, UWORD count) OLDCALL;
-//void mymemcpy(UBYTE *dest, const UBYTE *source, UWORD count) OLDCALL;
 
 #endif
